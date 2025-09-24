d71988bd052fce3a41c110a13c1c87fa40e34ef7
ALUAN WINDSON CIRYNO DA SILVA <Aluan.silva@senac.aluno.rio>
2025-09-24 19:06:00 -0300

    Atividade imagem completa

--- atividade1.py
+++ atividade1.py
@@ -9,4 +9,20 @@ class imagem:
 #Teste de classe da imagem
 imagem1 = imagem("Museu do amanhã", 2024)     
 
-imagem1.exibir()
+#Método de clase(usa CLS)
+#Não precisa da criação de instância
+
+@classmethod
+
+def mudar_nome(cls,nome_novo):
+    cls.nome = nome_novo
+    print(f"Agora o nome da imagem é: {cls.nome}, o museu futurístico")
+
+#Método escrito(não usa SELF nem CLS)    
+#Acesso - Não precisa da criação de instância
+#Escopo - Não recebe o SELF nem o CLS, então não tem acesso.
+#Caso queira acessar os da Classe poderá, porém terá que passar explicitamente
+
+@staticmethod
+def calcular_idade_imagem(ano_imagem, ano_atual):
+    return ano_atual - ano_imagem
